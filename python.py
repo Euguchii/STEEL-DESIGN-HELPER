dd2dd784f0c50b89d41a002f0d560fc87dfa396e
GABBY <142254455+Euguchii@users.noreply.github.com>
2025-04-18 12:00:15 +0800

    Add files via upload

---
+++ python.py
@@ -0,0 +1,13 @@
+print("hi")
+
+
+
+import pandas as pd
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel("ASTMSPEC.xlsx")  # Replace with your file path
+
+# Now df is a variable holding the Excel data
+print("hi")
+print(df)
+
